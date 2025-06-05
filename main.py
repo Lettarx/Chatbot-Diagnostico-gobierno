bd5dacf7a5ff7c261cd128f5e85daf5de92def91
Alejandra Codocedo <lettar.hyuga@gmail.com>
2025-06-05 17:51:02 -0400

    modificación nombres

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from page_empresa import log_empresas
-from page_solex import log_solex
+from empresa import mostrar_vista_empresa
+from solex import mostrar_vista_solex
 
 st.set_page_config(
     page_title="Entrevista Pre Diagnóstico",
@@ -20,8 +20,8 @@ def login():
         st.title("Iniciar Sesión")
         st.write("Por favor, introduce tus credenciales para continuar.")
         
-        usuario = st.text_input("Introduce tu nombre de usuario:")
-        contraseña = st.text_input("Introduce tu contraseña:", type="password")
+        usuario = st.text_input("Usuario:")
+        contraseña = st.text_input("Contraseña:", type="password")
         submit_button = st.form_submit_button("Iniciar Sesión")
         
         if submit_button:
@@ -42,8 +42,9 @@ if __name__ == "__main__":
     if not st.session_state.logged_in:
         login()
     else:
+        # Mostrar la vista correspondiente según el tipo de usuario
         if st.session_state.user_type == "empresa":
-            log_empresas()
+            mostrar_vista_empresa()
         elif st.session_state.user_type == "solex":
-            log_solex()
+            mostrar_vista_solex()
 
---
+++ solex.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import os
+
+load_dotenv()
+
+def mostrar_vista_solex():
+    st.title("Bienvenido a Solex")
+    st.write("Esta es la página de inicio de Solex. Aquí puedes acceder a todas las funcionalidades del sistema.")
+    
+    # Recolectar reportes mongo
+    client = MongoClient(os.getenv("MONGO_URI"))
+    db = client["diagnosticos"]
+    coleccion = db["resultados"]
+
+    # Obtener TODOS los datos de una vez
+    informes_todo = list(coleccion.find({}))
+
+    with st.form("formulario_informes"):
+        st.write("### Informes de Empresas")
+        st.write("Selecciona un informe para ver los detalles.")
+        
+        informe_seleccionado = st.selectbox(
+            "Selecciona un informe de empresa", 
+            informes_todo, 
+            format_func=lambda x: f"{x['empresa']} - {x['fecha']}"
+        )
+        sub = st.form_submit_button("Cargar Informe")
+
+    if sub and informe_seleccionado:
+        st.subheader(f"Informe de {informe_seleccionado['empresa']} - {informe_seleccionado['fecha']}")
+        
+        st.write("### Respuestas:")
+        for respuesta in informe_seleccionado['respuestas']:
+            with st.expander(f"📝 {respuesta['pregunta']}"):
+                st.write(respuesta['respuesta'])
+        
+        st.write("### Informe Final:")
+        st.write(informe_seleccionado['informe'])
